062f68c2c8b5d232b460c5501a513012fe31e4ec
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-11-12 23:14:54 +0300

    Add files via upload

--- src/main/java/kodluyoruz/rentAcar1/business/abstracts/UserService.java
+++ src/main/java/kodluyoruz/rentAcar1/business/abstracts/UserService.java
@@ -1,9 +1,12 @@
 package kodluyoruz.rentAcar1.business.abstracts;
 
+import kodluyoruz.rentAcar1.business.requests.saveRequests.SaveUserRequestDto;
 import kodluyoruz.rentAcar1.business.responses.UserResponseDto;
 import java.util.List;
 
 public interface UserService {
 
     List<UserResponseDto> getAll();
+
+    void add(SaveUserRequestDto saveUserRequestDto, Integer userId );
 }
